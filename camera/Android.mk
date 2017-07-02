5ec799c43de7e883137863ccab7b2c0aad6e8b2e
chenzc <dazzworld@163.com>
2017-07-02 23:28:41 +0800

    update camera

--- camera/Android.mk
+++ camera/Android.mk
@@ -11,13 +11,4 @@ LOCAL_SRC_FILES     := NubiaCamera.apk
 LOCAL_CERTIFICATE   := shared
 LOCAL_OVERRIDES_PACKAGES := Camera2
 
-AV_LIBS := libavcodec.so libavfilter.so libavformat.so libavutil.so libnubia_delay_recorder.so
-AV_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/$(LOCAL_MODULE)/lib/arm64/,$(notdir $(AV_LIBS)))
-
-$(AV_SYMLINKS):
-	@mkdir -p $(dir $@)
-	ln -sf /system/lib64/$(notdir $@) $@
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(AV_SYMLINKS)
-
 include $(BUILD_PREBUILT)
