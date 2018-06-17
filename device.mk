684103816273ec638ceeadae5c61ae0e72eab5d2
chenzc <dazzworld@163.com>
2018-06-17 18:52:52 +0800

    nx569j: build camera shim

--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshims_camera_native
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
