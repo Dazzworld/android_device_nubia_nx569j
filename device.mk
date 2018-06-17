2a7336209d32d400d529389e5b98dc2044ccdd76
chenzc <dazzworld@163.com>
2018-06-17 11:07:20 +0800

    nx569j: use prebuilt camera hal

--- device.mk
+++ device.mk
@@ -87,10 +87,9 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.msm8952 \
-    libmm-qcamera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
